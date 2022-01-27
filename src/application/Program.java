e1cc38db2cd2920cbbd9bd51231697d1f8706921
ToninhoDev <toninho.dmas@protonmail.com>
2022-01-27 18:46:54 -0300

    Implementado a busca do departamento pelo Id

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -14,6 +17,17 @@ public class Program {
 		Seller seller = sellerDao.findById(5);
 		
 		System.out.println(seller);
+		System.out.println();
+		
+		System.out.println("===Teste 2: Seller findByDepartment ====");
+		
+		Department department = new Department(2,null);
+		List<Seller> list = sellerDao.findByDepartament(department);
+		
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 
 	}
 
